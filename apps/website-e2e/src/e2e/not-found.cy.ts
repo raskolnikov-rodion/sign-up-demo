0ce5088e0966fd1094a26e98e8ccf6402a678849
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2023-10-17 22:58:13 +0100

    feat(website): implement Not Found page functionality

--- apps/website-e2e/src/e2e/not-found.cy.ts
+++ apps/website-e2e/src/e2e/not-found.cy.ts
@@ -1,9 +1,16 @@
-import { getHeader } from '../support/not-found.po';
+import { getHeader, getMessage, getHomeLink } from '../support/not-found.po';
 
 describe('Not Found Page', () => {
   beforeEach(() => cy.visit('/this-page-does-not-exist'));
 
-  it('should display the Not Found header', () => {
+  it('should display the Not Found page elements', () => {
     getHeader().contains('Page not found!');
+    getMessage().should('exist');
+    getHomeLink().should('exist');
+  });
+
+  it('should redirect user to Home Page', () => {
+    getHomeLink().click();
+    cy.location('pathname').should('equal', '/');
   });
 });
