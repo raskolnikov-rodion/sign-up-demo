1f548d98664f55e839657efbcd7f0cf35f214375
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2023-10-17 17:59:38 +0100

    feat(home): create sign-up form

--- apps/website-e2e/src/support/home.po.ts
+++ apps/website-e2e/src/support/home.po.ts
@@ -1,1 +1,7 @@
-export const getHeader = () => cy.get('h1');
+export const getHeader = () => cy.get("[data-test='home-header']");
+export const getSignUpForm = () => cy.get("[data-test='sign-up-form']");
+export const getFirstNameInput = () =>
+  cy.get("[data-test='sign-up-first-name']");
+export const getLastNameInput = () => cy.get("[data-test='sign-up-last-name']");
+export const getEmailInput = () => cy.get("[data-test='sign-up-email']");
+export const getSignUpSubmit = () => cy.get("[data-test='sign-up-submit']");
