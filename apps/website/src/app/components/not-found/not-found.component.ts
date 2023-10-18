987b5cb87fdf06016ffc87db95b20cc24936beed
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2023-10-18 15:43:12 +0100

    feat(website): implement Not Found page layout

--- apps/website/src/app/components/not-found/not-found.component.ts
+++ apps/website/src/app/components/not-found/not-found.component.ts
@@ -5,6 +5,7 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   selector: 'gus-not-found',
   templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink],
 })
