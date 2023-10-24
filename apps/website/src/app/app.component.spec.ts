5f3242767f6d98ed4022bee7796a167a6d6b1d31
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2023-10-24 18:16:10 +0100

    feat(website): configure translation module

--- apps/website/src/app/app.component.spec.ts
+++ apps/website/src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [
+        AppComponent,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+      ],
     }).compileComponents();
   });
 
--- apps/website/src/app/app.component.ts
+++ apps/website/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
@@ -8,4 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly translate = inject(TranslateService);
+
+  ngOnInit() {
+    this.setupLanguage();
+  }
+
+  private setupLanguage() {
+    this.translate.setDefaultLang('en-US');
+  }
+}
