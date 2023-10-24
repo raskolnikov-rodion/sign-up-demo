bdb7c733a051f1fbc71b2a267ab4eddfe74282a6
raskolnikov-rodion <22417165+raskolnikov-rodion@users.noreply.github.com>
2023-10-24 18:19:59 +0100

    feat(website): add translations for not-found component

--- apps/website/src/app/components/not-found/not-found.component.ts
+++ apps/website/src/app/components/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
@@ -7,6 +8,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
 })
 export class NotFoundComponent {}
